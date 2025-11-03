911737756499cc942d2ad1eeeee7d9578786cbd0
ShAsHi <shashinarmala29@gmail.com>
2025-11-03 13:26:22 +0530

    Update main.tf

--- Terraform/AutoDeploymentResGKE/main.tf
+++ Terraform/AutoDeploymentResGKE/main.tf
@@ -1,1 +1,51 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "gke_cluster" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Cluster version (latest will pick current available version)
+  min_master_version = "latest"
+
+  node_config {
+    machine_type = "e2-standard-4"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 1
+
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 3
+    }
+
+    node_config {
+      machine_type = "e2-standard-4"
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+    }
+  }
+}
 
